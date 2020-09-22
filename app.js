c740969e206b983b15eed595df6e0dda7355b163
terahje <terahjequinn@gmail.com>
2020-09-21 21:48:13 -0700

    capture-input

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const printProfileData = profileDataArr => {
 
     console.log('================');
 
-profileDataArr.forEach(function(profileItem) {
-    console.log(profileItem);
-});
+profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
