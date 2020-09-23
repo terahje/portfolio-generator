a9d423814f6b32103442f1b804e9d3511e5da4d4
terahje <terahjequinn@gmail.com>
2020-09-22 21:39:39 -0700

    added generate the html file

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
 //console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+//const printProfileData = profileDataArr => {
+  //  for (let i = 0; i < profileDataArr.length; i += 1) {
+    //    console.log(profileDataArr[i]);
+    //}
 
-    console.log('================');
+    //console.log('================');
 
-profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
 
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
 
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw err;
+    
+    console.log('Portolio complete!  Check out index.html to see the output!');
+});
