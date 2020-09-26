9f2275071f8d369528e61c91ff869ece13edc203
terahje <terahjequinn@gmail.com>
2020-09-26 08:01:03 -0700

    update to make certain fields required, merging to develop

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
-const fs = require('fs');
+//const fs = require('fs');
 //const generatePage = require('./src/page-template.js');
 
 //const pageHTML = generatePage(name, github);
@@ -17,17 +17,44 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubUsername => {
+                if(githubUsername) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Username!');
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({confirmAbout}) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -46,12 +73,26 @@ if (!portfolioData.projects) {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: projectName => {
+                if(projectName) {
+                    return true;
+                } else {
+                    console.log('Please enter your projects name!');
+                }
+            }            
           },
           {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescription => {
+                if(projectDescription) {
+                    return true;
+                } else {
+                    console.log('Please describe your project!');
+                }
+            }
           },
           {
             type: 'checkbox',
@@ -62,7 +103,14 @@ if (!portfolioData.projects) {
           {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: githubLink => {
+                if(githubLink) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub link!');
+                }
+            }
           },
           {
             type: 'confirm',
